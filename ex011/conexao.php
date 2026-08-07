bba9ad6fa0187f56ae2073af8e0e5dd221436feb
Matheus Jorge <matheusjorgealves09@gmail.com>
2026-08-07 20:32:55 -0300

    atualizei os comentários do conexao.php

--- ex011/conexao.php
+++ ex011/conexao.php
@@ -2,12 +2,12 @@
 
     // variáveis que armazenam informações do meu banco de dados
     $servidor = "localhost"; // está rodando na mesma máquina
-    $usuario = "root"; // root dá acesso a praticamente tudo 
+    $usuario = "root"; // usuário administrador padrão do MySQL
     $senha = ""; // senha do root é vazia
     $banco = "cadastro_produtos"; // nome do banco
     $porta = 3307; // porta do MySql
 
-    // usei isso somente para desabilitar a mensagem de erro o mysql e permitir testar algumas coisas
+    // desativa o relatório automático de erros
     mysqli_report(MYSQLI_REPORT_OFF);
 
     // conexão com o banco de dados
