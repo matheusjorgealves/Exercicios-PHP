5c48da2c3acb669782317fa49fa07500c59eaf7a
Matheus Jorge <matheusjorgealves09@gmail.com>
2026-09-23 17:18:52 -0300

    ex002. Verifica se existe um usuáro de mesmo e-mail já cadastrado e impede que dois usuários tenham o mesmo e-mail

--- ex013-Validacoes/ex002-VerificacaoUnicidade/ex002.php
+++ ex013-Validacoes/ex002-VerificacaoUnicidade/ex002.php
@@ -8,48 +8,19 @@
     $email = "";
     $idade = 0;
     $senha = "";
+    $msgInicial = "Cadastre-se no formulário abaixo";
     $verifEmail = false;
     $erros = [];
     $armazemEmailSQL = [];
+    $cadastro = false;
 
     // se o método da requisição for post
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // recebendo as respostas do form
         $nome = trim($_POST["nome"]);
         $email = trim($_POST["email"]);
         $idade = $_POST["idade"];
         $senha = trim($_POST["senha"]);
-
-        //  ----- SELECT email -----
-        // variável para armazenar instrução sql
-        $sql = "SELECT * FROM usuarios where email = ?;";
-
-        // statement preparado para a associação de dados aos placeholders ?
-        $stmt = mysqli_prepare($conexao, $sql);
-
-        // associando dados aos placeholders do stmt
-        mysqli_stmt_bind_param($stmt, "s",$email);
-
-        // executando o stmt
-        $execucao = mysqli_stmt_execute($stmt);
-
-        // se a execução do stmt falhar
-        if ($execucao === false) {
-            $erros[] = "Erro: ". mysqli_stmt_error($stmt);
-        } else {
-            // recebendo o resultado da consulta (conjunto de registros)
-            $resultadoSelect = mysqli_stmt_get_result($stmt);
-
-            // enquanto houverem registros
-            while ($emailSQL = mysqli_fetch_assoc($resultadoSelect)) {
-                $armazemEmailSQL[] = $emailSQL;
-            }
-
-            // se email for único
-            if (empty($armazemEmailSQL)) {
-                $verifEmail = true;
-            }
-        }
 
         // VALIDAÇÕES - não pode null
         // nome - mínimo 3 caracteres
@@ -62,8 +33,6 @@
             $erros[] = "E-mail inválido!";
         } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
             $erros[] = "Formato do e-mail inválido!";
-        } elseif ($verifEmail === false) {
-            $erros[] = "E-mail já existente!";
         }
 
         // idade - 18 a 100 anos de idade
@@ -78,7 +47,46 @@
             $erros[] = "Senha inválida! Mínimo 8 caracteres";
         }
 
+        //  ----- SELECT email -----
 
+        // se não existirem erros
+        if (empty($erros)) {
+            // variável para armazenar instrução sql
+            $sql = "SELECT * FROM usuarios where email = ?;";
+
+            // statement preparado para a associação de dados aos placeholders ?
+            $stmt = mysqli_prepare($conexao, $sql);
+
+            // associando dados aos placeholders do stmt
+            mysqli_stmt_bind_param($stmt, "s",$email);
+
+            // executando o stmt
+            $execucao = mysqli_stmt_execute($stmt);
+
+            // se a execução do stmt falhar
+            if ($execucao === false) {
+                $erros[] = "Erro: ". mysqli_stmt_error($stmt);
+            } else {
+                // recebendo o resultado da consulta (conjunto de registros)
+                $resultadoSelect = mysqli_stmt_get_result($stmt);
+
+                // enquanto houverem registros
+                while ($emailSQL = mysqli_fetch_assoc($resultadoSelect)) {
+                    $armazemEmailSQL[] = $emailSQL;
+                }
+
+                // se email for único
+                if (empty($armazemEmailSQL)) {
+                    $verifEmail = true;
+                }
+            }
+        }
+
+        // se o e-mail já existir
+        if ($verifEmail === false) {
+            $erros[] = "E-mail já existente!";
+        }
+        
         // se não houverem erros
         if (empty($erros)) {
             // ----- INSERT -----
@@ -92,12 +100,23 @@
             // associando os dados aos placeholders
             mysqli_stmt_bind_param($stmt, "ssis",$nome, $email, $idade, $senha);
 
-            // continuação ... stmt_execute
+            // executando o stmt 
+            $execucao = mysqli_stmt_execute($stmt);
+
+            // se houver erro na execução
+            if ($execucao === false) {
+                $erros[] = "Erro:". mysqli_stmt_error($stmt);
+            } else {
+                // cadastro deu certo 
+                $cadastro = true;
+
+                // armazenando o resultado do id do último usuário criado no banco
+                $idUsuario = mysqli_insert_id($conexao);
+            }
         }
     }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -107,20 +126,31 @@
     <title>Cadastro de usuários</title>
 </head>
 <body>
+    <!-- cabeçalho -->
     <header>
         <h1>Cadastro de usuários</h1>
     </header>
 
+    <!-- conteúdo principal -->
     <main>
-        <p>Cadastre-se no formulário abaixo</p>
-
         <?php
+            // se o cadastro foi concluído
+            if ($cadastro === true) {
+                // mudando a mensagem inicial
+                $msgInicial = "Id do usuário cadastrado:". $idUsuario;
+            }
+
+            // mensagem inicial
+            ?>
+            <p><?= $msgInicial ?></p>
+            <?php
+
             // se houverem erros
             if (!empty($erros)) {
                 // percorrendo  os erros
                 foreach ($erros as $erro) {
                     ?>
-                    <p>Erro: <?= $erro ?></p>
+                    <p><mark>Erro: <?= $erro ?></mark></p>
                     <?php
                 }
             }
@@ -129,22 +159,22 @@
         <form action="" method="post">
             <!-- nome -->
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome" required value="<?= $_SERVER["REQUEST_METHOD"] == "POST" ? $nome : "" ?>">
+            <input type="text" name="nome" id="nome" required value="<?= $_SERVER["REQUEST_METHOD"] === "POST" && $cadastro === false ? $nome : "" ?>">
             <br> <br>
 
             <!-- email -->
             <label for="email">E-mail:</label>
-            <input type="email" name="email" id="email" required value="<?= $_SERVER["REQUEST_METHOD"] == "POST" ? $email : "" ?>">
+            <input type="email" name="email" id="email" required value="<?= $_SERVER["REQUEST_METHOD"] === "POST" && $cadastro === false ? $email : "" ?>">
             <br> <br>
 
             <!-- idade -->
             <label for="idade">Idade:</label>
-            <input type="number" name="idade" id="idade" min="18" max="100" required value="<?= $_SERVER["REQUEST_METHOD"] == "POST" ? $idade : "" ?>">
+            <input type="number" name="idade" id="idade" min="18" max="100" required value="<?= $_SERVER["REQUEST_METHOD"] === "POST" && $cadastro === false ? $idade : "" ?>" >
             <br> <br>
 
             <!-- senha -->
             <label for="senha">Senha:</label>
-            <input type="password" name="senha" id="senha" required value="<?= $_SERVER["REQUEST_METHOD"] == "POST" ? $senha : "" ?>">
+            <input type="password" name="senha" id="senha" required value="<?= $_SERVER["REQUEST_METHOD"] === "POST" && $cadastro === false ? $senha : "" ?>">
             <br> <br>
 
             <!-- botão cadastrar -->
@@ -152,6 +182,7 @@
         </form>
     </main>
 
+    <!-- rodapé -->
     <footer>
         <p>Site desenvolvido por <a href="https://github.com/matheusjorgealves" target="blanck">Matheus Jorge</a></p>
     </footer>
